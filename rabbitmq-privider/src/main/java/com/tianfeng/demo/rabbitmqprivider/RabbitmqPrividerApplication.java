4bf6436a22b11fcf9438194be29348cf56aded8f
mengxuegutianfeng <2237878365@qq.com>
2020-03-21 22:11:13 +0800

    rabbitmq-privider

---
+++ rabbitmq-privider/src/main/java/com/tianfeng/demo/rabbitmqprivider/RabbitmqPrividerApplication.java
@@ -0,0 +1,13 @@
+package com.tianfeng.demo.rabbitmqprivider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqPrividerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqPrividerApplication.class, args);
+    }
+
+}
---
+++ rabbitmq-privider/src/test/java/com/tianfeng/demo/rabbitmqprivider/RabbitmqPrividerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianfeng.demo.rabbitmqprivider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqPrividerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
